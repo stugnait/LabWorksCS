3f0c1948e18e458c4394c2e55097e0c73450771a
Igor Teslitskii <i.teslitskii@gmail.com>
2023-09-27 20:19:22 +0300

    27/09

---
+++ CSLabWork14/CSLabWork14/SecondWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace CSLabWork14;
+
+public partial class SecondWindow : Window
+{
+    public SecondWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ CSLabWork14/CSLabWork14/ThirdWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace CSLabWork14;
+
+public partial class ThirdWindow : Window
+{
+    public ThirdWindow()
+    {
+        InitializeComponent();
+    }
+}
